9da3caf843e6cdfdcdbf1086adefcd24af79de2b
vladoja <dr.jalo2@gmail.com>
2023-05-17 16:39:27 +0200

    Stack Widget with FadeInImage

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -34,7 +36,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: ((ctx, index) => Text(meals[index].title)),
+        itemBuilder: ((ctx, index) => MealItem(meal: meals[index])),
       );
     }
     return Scaffold(
