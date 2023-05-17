bd12e4dccd52c0a9b72f4532c8ac97f3244ffdca
vladoja <dr.jalo2@gmail.com>
2023-05-17 10:42:38 +0200

    categoryItem and displaying dummy data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: const Text('Pick Your Category')),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
-            mainAxisExtent: 20),
+            mainAxisSpacing: 20),
         children: [
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
