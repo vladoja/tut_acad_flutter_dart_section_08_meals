148ee34d1210298d4b77663f24046e6c28e5ddbf
vladoja <dr.jalo2@gmail.com>
2023-05-18 15:29:14 +0200

    Back back button

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/meals.dart';
-import 'package:meals/screens/tabs.dart';
-import 'package:meals/widgets/main_drawer.dart';
+// import 'package:meals/screens/meals.dart';
+// import 'package:meals/screens/tabs.dart';
+// import 'package:meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -21,13 +21,13 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      drawer: MainDrawer(onSelectScreen: (identifier) {
-        Navigator.of(context).pop();
-        if (identifier == 'meals') {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (ctx) => const TabsScreen()));
-        }
-      }),
+      // drawer: MainDrawer(onSelectScreen: (identifier) {
+      //   Navigator.of(context).pop();
+      //   if (identifier == 'meals') {
+      //     Navigator.of(context)
+      //         .push(MaterialPageRoute(builder: (ctx) => const TabsScreen()));
+      //   }
+      // }),
       body: Column(
         children: [
           SwitchListTile(
