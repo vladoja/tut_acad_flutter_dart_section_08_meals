04a747d604e21a1c0e4b39fae998f0cf62ddd020
vladoja <dr.jalo2@gmail.com>
2023-05-18 13:17:15 +0200

    ListTile in Drawer.

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -9,28 +9,57 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           DrawerHeader(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Theme.of(context).colorScheme.primaryContainer,
-                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8)
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-              child: Row(
-                children: [
-                  Icon(Icons.fastfood,
-                      size: 48, color: Theme.of(context).colorScheme.primary),
-                  const SizedBox(
-                    width: 18,
-                  ),
-                  Text(
-                    'Test',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(color: Theme.of(context).colorScheme.primary),
-                  )
-                ],
-              ))
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Theme.of(context).colorScheme.primaryContainer,
+              Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+            child: Row(
+              children: [
+                Icon(Icons.fastfood,
+                    size: 48, color: Theme.of(context).colorScheme.primary),
+                const SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Test',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 24),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.settings,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 24),
+            ),
+          ),
         ],
       ),
     );
